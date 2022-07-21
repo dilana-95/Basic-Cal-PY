fccf8e1ed67aba9fca8232dcd0be11ae249b2b24
Dilana Sachintha <90304982+dilana-95@users.noreply.github.com>
2022-07-22 03:41:36 +0530

    new relay

--- main.py
+++ main.py
@@ -1,57 +1,160 @@
-def add(x, y):
-    return x + y
+def add(a, b):
+    return a + b
 
 
-def subtract(x, y):
-    return x - y
+def subtract(a, b):
+    return a - b
 
 
-def multiply(x, y):
-    return x * y
+def multiply(a, b):
+    return a * b
 
 
-def divide(x, y):
-    return x / y
+def divide(a, b):
+    try:
+
+        return a / b
+
+    except Exception as e:
+
+        print(e)
 
 
-def modulus(x, y):
-    return x % y
+def power(a, b):
+    return a ** b
 
 
-print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
-print("5.Modulus")
+def remainder(a, b):
+    return a % b
+
+
+def select_op(choice):
+    if (choice == '#'):
+
+        return -1
+
+    elif (choice == '$'):
+
+        return 0
+
+    elif (choice in ('+', '-', '*', '/', '^', '%')):
+
+        while (True):
+
+            num1s = str(input("Enter first number: "))
+
+            print(num1s)
+
+            if num1s.endswith('$'):
+                return 0
+
+            if num1s.endswith('#'):
+                return -1
+
+            try:
+
+                num1 = float(num1s)
+
+                break
+
+            except:
+
+                print("Not a valid number,please enter again")
+
+                continue
+
+        while (True):
+
+            num2s = str(input("Enter second number: "))
+
+            print(num2s)
+
+            if num2s.endswith('$'):
+                return 0
+
+            if num2s.endswith('#'):
+                return -1
+
+            try:
+
+                num2 = float(num2s)
+
+                break
+
+            except:
+
+                print("Not a valid number,please enter again..")
+
+                continue
+
+        if choice == '+':
+
+            print(num1, "+", num2, "=", add(num1, num2))
+
+
+
+        elif choice == '-':
+
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+
+
+        elif choice == '*':
+
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+
+
+        elif choice == '/':
+
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+        elif choice == '^':
+
+            print(num1, "^", num2, "=", power(num1, num2))
+
+        elif choice == '%':
+
+            print(num1, "%", num2, "=", remainder(num1, num2))
+
+        else:
+
+            print("Something Went Wrong!!")
+
+    else:
+
+        print("Unrecognized operation")
+
 
 while True:
 
-    choice = input("Enter choice(1/2/3/4/5): ")
+    print("Select operation..")
 
-    # check if choice is one of the four options
-    if choice in ('1', '2', '3', '4', '5'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
+    print("1.Add      : + ")
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+    print("2.Subtract : - ")
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+    print("3.Multiply : * ")
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+    print("4.Divide   : / ")
 
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+    print("5.Power    : ^ ")
 
-        elif choice == '5':
-            print(num1, "%", num2, "=", modulus(num1, num2))
+    print("6.Remainder: % ")
 
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
-            break
+    print("7.Terminate: # ")
 
-    else:
-        print("Invalid Input! Try Again")
+    print("8.Reset    : $ ")
+
+    # take input from the user
+
+    choice = input("Enter choice(+,-,*,/,^,%,#,$): ")
+
+    print(choice)
+
+    if (select_op(choice) == -1):
+        # program ends here
+
+        print("Done. Terminating")
+
+        exit()
